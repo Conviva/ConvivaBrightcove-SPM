29e251bbf5f3a7540419dd0a69e51ec062cbd0dd
smadinen <smadine@conviva.com>
2021-03-01 16:20:49 +0530

    Package file for 4.0.4

---
+++ Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "ConvivaBrightcove",
+    platforms: [
+        .iOS(.v12)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "ConvivaBrightcove",
+            targets: ["ConvivaBrightcoveTarget"])
+    ],
+    targets: [
+        .binaryTarget(
+            name: "ConvivaBrightcove",
+            url: "https://github.com/Conviva/ConvivaBrightcove/raw/master/Framework/ConvivaBrightcove/ConvivaBrightcove.xcframework.zip",
+            checksum: "5b8a0cf501f81fb212c7e1b7f6b94ea85d37f328279050ddf81e61e0c7323e35"),
+        
+        .binaryTarget(
+            name: "ConvivaSDK",
+            url: "https://github.com/Conviva/ConvivaSDK/raw/master/Framework/ConvivaSDK.xcframework.zip",
+            checksum: "ca0b36f01d9349c8c18c219701076a52b26aff5f86a8d9435b15bd555197ed00"),
+
+        .target(
+              name: "ConvivaBrightcoveTarget",
+              dependencies: [
+                .target(name: "ConvivaSDK"),
+                .target(name: "ConvivaBrightcove")
+              ],
+              path: "PlatformExcludes"
+            )
+     ]
+)
