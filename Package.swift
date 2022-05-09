dbc8937de0413d22f9c1b01ae72e0a44131719a1
smadinen <51313354+smadinen@users.noreply.github.com>
2022-05-09 15:50:53 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ConvivaBrightcove",
-            url: "https://github.com/Conviva/ConvivaBrightcove/raw/4.0.14/Framework/ConvivaBrightcove/ConvivaBrightcove.xcframework.zip",
-            checksum: "d7b4a74c65e0567b700633ab89f191f971fc236f4d512c346ee02ac294568021"),
+            url: "https://github.com/Conviva/ConvivaBrightcove/raw/4.0.15/Framework/ConvivaBrightcove/ConvivaBrightcove.xcframework.zip",
+            checksum: "dc849e4508e06353e9c7032f8853aba8bffb8bdae927477d93103b1b56a8e458"),
         
         .binaryTarget(
             name: "ConvivaSDK",
-            url: "https://github.com/Conviva/ConvivaSDK/raw/4.0.26/Framework/ConvivaSDK.xcframework.zip",
-            checksum: "5a4c385fc52f2f4c6a02c6c345af81aa6ff836750080f856d702b7f3834c9a15"),
+            url: "https://github.com/Conviva/ConvivaSDK/raw/4.0.27/Framework/ConvivaSDK.xcframework.zip",
+            checksum: "d8e1e443061d8a07a3799bf0b5afabbf0f48dae21992ad9402ac8804f1ec3e6d"),
 
         .target(
               name: "ConvivaBrightcoveTarget",
