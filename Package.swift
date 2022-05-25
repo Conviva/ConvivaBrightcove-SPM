3ef90d84241211383317a87b56a7e84f0b590830
Butchi Peddi <58072398+butchi-conviva@users.noreply.github.com>
2022-05-25 12:13:17 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "ConvivaBrightcove",
-            url: "https://github.com/Conviva/ConvivaBrightcove/raw/4.0.15/Framework/ConvivaBrightcove/ConvivaBrightcove.xcframework.zip",
-            checksum: "dc849e4508e06353e9c7032f8853aba8bffb8bdae927477d93103b1b56a8e458"),
+            url: "https://github.com/Conviva/ConvivaBrightcove/raw/4.0.16/Framework/ConvivaBrightcove/ConvivaBrightcove.xcframework.zip",
+            checksum: "7c65f9e50dce493d7c5b97604220f44dd9df9de79935a2055de514704a6fefd6"),
         
         .binaryTarget(
             name: "ConvivaSDK",
-            url: "https://github.com/Conviva/ConvivaSDK/raw/4.0.27/Framework/ConvivaSDK.xcframework.zip",
-            checksum: "d8e1e443061d8a07a3799bf0b5afabbf0f48dae21992ad9402ac8804f1ec3e6d"),
+            url: "https://github.com/Conviva/ConvivaSDK/raw/4.0.26/Framework/ConvivaSDK.xcframework.zip",
+            checksum: "5a4c385fc52f2f4c6a02c6c345af81aa6ff836750080f856d702b7f3834c9a15"),
 
         .target(
               name: "ConvivaBrightcoveTarget",
